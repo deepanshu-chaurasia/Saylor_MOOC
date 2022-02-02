7ff77ffaa400b372f29ff65c2f59dce7899d8c36
Deepanshu Chaurasia <chaurasia.deepanshu03@gmail.com>
2022-02-02 20:57:26 +0530

    Practice on Strings.

---
+++ Practice on Strings/Excercises/Ex_1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+    string nam = "My name is deepanshu chaurasia";
+    int n = 1;
+
+    for (int i = 0; i < nam.size(); i++) {
+
+        if (nam[i] == ' ' || nam[i] == '\n' || nam[i] == '\t') {
+            n++;
+        }
+    }
+    cout << "Number of words: " << n << endl;
+      cin.get();
+}
+

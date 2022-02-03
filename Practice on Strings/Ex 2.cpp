63ec829b4a5254d5e236f837841ba4c6bc905b4a
Deepanshu Chaurasia <chaurasia.deepanshu03@gmail.com>
2022-02-04 00:05:06 +0530

    Ex 2

--- Practice on Strings/Ex 2.cpp
+++ Practice on Strings/Ex 2.cpp
@@ -6,21 +6,32 @@ int main() {
     cout << "Check the length of two strings:" << endl;
     cout << "------------------------------------" << endl;
 
-    char nam[100];
-    char* n = nam;
+    char nam[20];
     cout << "Input the 1st string: ";
     cin >> nam;
 
-    char nam2[100];
+    char nam2[20];
     cout << "Input the 2nd string: ";
     cin >> nam2;
-    char* m = nam2;
 
-    if (*n == *m) {
-        cout << "Strings are Equal" << endl;
+    cout << nam << " " << nam2 <<endl;
+    
+    int n = 0;
+    
+    for (int i = 0; i < sizeof(nam); i++) {
+        if (nam[i] == nam2[i]) {
+            n++;
+        }
+    }
+    if (n == 20) {
+        cout << "same words!" << endl;
+
     }
     else {
-        cout << "Strings are not equal" << endl;
+        cout << "different words" << endl;
+
     }
     cin.get();
 }
+
+

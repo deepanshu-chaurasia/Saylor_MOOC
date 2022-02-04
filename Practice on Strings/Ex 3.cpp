a32a067783e2123a5ead4418f30b2a25b7e65335
Deepanshu Chaurasia <chaurasia.deepanshu03@gmail.com>
2022-02-04 15:27:41 +0530

    Practice on Strings

---
+++ Practice on Strings/Ex 3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+   string str;
+   getline(cin, str);
+    int i, alp, dgt, spch;
+    i = 0;
+    alp = 0;
+    dgt = 0;
+    spch = 0;
+    cout << str;
+
+    for (i = 0; i < str.size();i++) {
+        if (str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z') {
+            alp++;
+        }
+        else if (str[i] >= '0' && str[i] <= '9') {
+            dgt++;
+        }
+        else {
+            spch++;
+        }
+    }
+    cout << "Number of Alphabets in the string is : " << alp << endl;
+    cout << "Number of Digits in the string is : " << dgt << endl;
+    cout << "Number of Special characters in the string is : " << spch << endl;
+    cin.get();
+}
